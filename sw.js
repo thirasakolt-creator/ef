72e3bf49a269a3f5a6f3df0e23ba3057e5011aad
thirasakolt-creator <thirasakolt@gmail.com>
2026-09-08 13:03:37 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,45 @@
-const CACHE = 'wm-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json',
-                './icons/icon-192.png', './icons/icon-512.png'];
+const CACHE = 'wm-v3';
+const ASSETS = [
+  './', './index.html', './styles.css', './app.js',
+  './manifest.json', './icons/icon-192.png', './icons/icon-512.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.allSettled(ASSETS.map(a => c.add(a))))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.hostname.includes('script.google.com')) return; // ไม่ cache API
-  if (e.request.method !== 'GET') return;
+self.addEventListener('fetch', function (e) {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+
+  const url = new URL(req.url);
+  if (url.hostname.indexOf('script.google') !== -1) return;
+  if (url.origin !== self.location.origin) return;
+
   e.respondWith(
-    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(req).then(function (hit) {
+      if (hit) return hit;
+      return fetch(req).then(function (res) {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(req, clone));
+        }
+        return res;
+      }).catch(function () {
+        return caches.match('./index.html');
+      });
+    })
   );
 });
