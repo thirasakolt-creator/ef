2f24e2c4d2620404d34afc32a71e18d43641de13
thirasakolt-creator <thirasakolt@gmail.com>
2026-09-14 09:48:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wm-v10';
+const CACHE = 'wm-v11';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js',
   './manifest.json', './icons/icon-192.png', './icons/icon-512.png'
